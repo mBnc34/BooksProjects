c39cf436aa600ef15233387543ba8bab899dfcc9
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-10-01 20:59:22 +0200

    [Book-5] : init first activities for test the launch

---
+++ app/src/main/java/es/usj/booksprojects/activity/BookListActivity.java
@@ -0,0 +1,19 @@
+package es.usj.booksprojects.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.usj.booksprojects.R;
+
+
+
+public class BookListActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
