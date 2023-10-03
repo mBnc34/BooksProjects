a751bd4ed91570687a74cdcaa0f49bd6309c1c11
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-10-03 16:46:30 +0200

    [Book-6] : listview of books(using mocks)

--- app/src/main/java/es/usj/booksprojects/activity/SplashActivity.java
+++ app/src/main/java/es/usj/booksprojects/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package es.usj.booksprojects.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import es.usj.booksprojects.R;
@@ -12,5 +13,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent intent  = new Intent(this, BookListActivity.class);
+        this.startActivity(intent);
     }
 }
