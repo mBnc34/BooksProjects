e97b50a03deee528979abbee7d1231e00aabf0d6
mBnc34 <104487495+mBnc34@users.noreply.github.com>
2023-11-10 20:49:51 +0100

    [Book-13] : see the HTTP data in activity

--- app/src/main/java/es/usj/booksprojects/activity/BookListActivity.java
+++ app/src/main/java/es/usj/booksprojects/activity/BookListActivity.java
@@ -1,61 +1,55 @@
 package es.usj.booksprojects.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
-import java.io.IOException;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 import es.usj.booksprojects.R;
 import es.usj.booksprojects.adapters.BookListAdapter;
 import es.usj.booksprojects.data.BookData;
-import es.usj.booksprojects.mocks.BookMockData;
 import es.usj.booksprojects.model.Book;
 import es.usj.booksprojects.serverOperations.GetRequest;
-
 
 public class BookListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView recyclerView2;
-
     private RecyclerView recyclerView3;
     private int cardViewId = R.layout.view_book_card;
     private BookListAdapter adapter;
 
-    private GetRequest getRequest = new GetRequest();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        BookData books = BookData.getInstance();
-        try {
-            books.setBooks(getRequest.retrBooks("judo"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
 
-        List<Book> localDataSet= BookMockData.getMockBooks();
-        recyclerView = findViewById(R.id.rvBooks);
-        adapter = new BookListAdapter(cardViewId, books.getBooks());
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.setAdapter(adapter);
+        GetRequest getRequest = new GetRequest();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView2 = findViewById(R.id.rvYourList);
-        recyclerView2.setLayoutManager(layoutManager);
-        recyclerView2.setAdapter(adapter);
+        getRequest.retrBooks("math", new GetRequest.Callback() {
+            @Override
+            public void onSuccess(List<Book> books) {
+                BookData.getInstance().setBooks(books);
 
-        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView3 = findViewById(R.id.rvNewBooks);
-        recyclerView3.setLayoutManager(layoutManager2);
-        recyclerView3.setAdapter(adapter);
+                recyclerView = findViewById(R.id.rvBooks);
+                recyclerView2 = findViewById(R.id.rvNewBooks);
+                recyclerView3 = findViewById(R.id.rvYourList);
+                adapter = new BookListAdapter(cardViewId, books);
+                recyclerView.setLayoutManager(new GridLayoutManager(BookListActivity.this, 2));
+                recyclerView.setAdapter(adapter);
+                recyclerView2.setLayoutManager(new GridLayoutManager(BookListActivity.this, 2));
+                recyclerView2.setAdapter(adapter);
+                recyclerView3.setLayoutManager(new GridLayoutManager(BookListActivity.this, 2));
+                recyclerView3.setAdapter(adapter);
+            }
 
+            @Override
+            public void onFailure(Throwable t) {
+                // Handle failure
+            }
+        });
     }
-
 }
--- app/src/main/java/es/usj/booksprojects/serverOperations/GetRequest.java
+++ app/src/main/java/es/usj/booksprojects/serverOperations/GetRequest.java
@@ -1,6 +1,12 @@
 package es.usj.booksprojects.serverOperations;
 
+import android.os.AsyncTask;
+
 import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import es.usj.booksprojects.mapper.BookMapper;
 import es.usj.booksprojects.model.Book;
@@ -9,40 +15,61 @@ import es.usj.booksprojects.serverOperations.valueApi.BooksApIResponse;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class GetRequest {
-    private final String QUERY = "https://openlibrary.org/search.json?q=";
 
-    public List<Book> retrBooks(String searchName) throws IOException {
-        String bookQuery = QUERY.concat(searchName);
-        OkHttpClient client = new OkHttpClient();
-        List<Book> booksList = new ArrayList<>();
+    private static final String QUERY = "https://openlibrary.org/search.json?q=";
 
-        // Créer une requête GET avec l'URL
-        Request request = new Request.Builder()
-                .url(bookQuery)
-                .build();
+    public interface Callback {
+        void onSuccess(List<Book> books);
 
-        // Exécuter la requête de manière synchrone
-        Response response = client.newCall(request).execute();
+        void onFailure(Throwable t);
+    }
 
-        if (response.isSuccessful()) {
-            String jsonResponse = response.body().string();
-            Gson gson = new Gson();
-            BooksApIResponse booksResponse = gson.fromJson(jsonResponse, BooksApIResponse.class);
+    public void retrBooks(String searchName, Callback callback) {
+        new AsyncTask<Void, Void, List<Book>>() {
+            @Override
+            protected List<Book> doInBackground(Void... voids) {
+                try {
+                    String bookQuery = QUERY.concat(searchName);
+                    OkHttpClient client = new OkHttpClient();
+                    List<Book> booksList = new ArrayList<>();
 
-            // Vérifier si la réponse contient des livres
-            if (booksResponse != null && booksResponse.getBooks() != null) {
-                for (BookValueApi bookApi : booksResponse.getBooks()) {
-                    Book book = BookMapper.toDomainBook(bookApi);
-                    booksList.add(book);
+                    // Créer une requête GET avec l'URL
+                    Request request = new Request.Builder()
+                            .url(bookQuery)
+                            .build();
+
+                    // Exécuter la requête de manière synchrone
+                    Response response = client.newCall(request).execute();
+
+                    if (response.isSuccessful()) {
+                        String jsonResponse = response.body().string();
+                        Gson gson = new Gson();
+                        BooksApIResponse booksResponse = gson.fromJson(jsonResponse, BooksApIResponse.class);
+
+                        // Vérifier si la réponse contient des livres
+                        if (booksResponse != null && booksResponse.getBooks() != null) {
+                            for (BookValueApi bookApi : booksResponse.getBooks()) {
+                                Book book = BookMapper.toDomainBook(bookApi);
+                                booksList.add(book);
+                            }
+                            return booksList;
+                        }
+                    }
+                } catch (IOException e) {
+                    callback.onFailure(e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<Book> books) {
+                super.onPostExecute(books);
+                if (books != null) {
+                    callback.onSuccess(books);
                 }
             }
-        }
-
-        return booksList;
+        }.execute();
     }
 }
