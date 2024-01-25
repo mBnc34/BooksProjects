7c3dfc39eeb124432ca0af1ad25a2bc6c75c41ff
LeroyUA62 <damienleroy62111@gmail.com>
2024-01-25 10:53:16 +0100

    editText de SearchActivity+retrieveAuthor

--- app/src/main/java/es/usj/booksprojects/activity/SearchActivity.java
+++ app/src/main/java/es/usj/booksprojects/activity/SearchActivity.java
@@ -7,8 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,17 +20,21 @@ import java.util.List;
 import es.usj.booksprojects.R;
 import es.usj.booksprojects.adapters.BookListAdapter;
 import es.usj.booksprojects.adapters.SearchBookListAdapter;
+import es.usj.booksprojects.data.AuthorData;
 import es.usj.booksprojects.data.BookData;
 import es.usj.booksprojects.database.AppDatabase;
 import es.usj.booksprojects.database.BookDao;
+import es.usj.booksprojects.model.Author;
 import es.usj.booksprojects.model.Book;
 import es.usj.booksprojects.serverOperations.GetRequest;
+import es.usj.booksprojects.serverOperations.callback.AuthorGetRequestCallback;
 import es.usj.booksprojects.serverOperations.callback.BookGetRequestCallback;
 
 public class SearchActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Button validerButton;
-    private SearchView searchView;
+    //private SearchView searchView;
+    private EditText searchEditText;
     private TextView searchTextView;
     private BookListAdapter adapter;
     private BookData searchResults;
@@ -40,7 +46,8 @@ public class SearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        searchView = findViewById(R.id.searchView);
+        //searchView = findViewById(R.id.searchView);
+        searchEditText = findViewById(R.id.searchEditText);
         validerButton = findViewById(R.id.validerButton);
         recyclerView = findViewById(R.id.recyclerView);
         searchTextView = findViewById(R.id.searchTextView);
@@ -65,7 +72,8 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void performSearch() {
-        String searchQuery = searchView.getQuery().toString();
+        //String searchQuery = searchView.getQuery().toString();
+        String searchQuery = searchEditText.getText().toString();
 
         if (!TextUtils.isEmpty(searchQuery)) {
             GetRequest getRequest = new GetRequest();
@@ -76,6 +84,29 @@ public class SearchActivity extends AppCompatActivity {
                     searchResults.setBooks(books);
                     adapter.updateBooks(books);  // Use the new method to update data
                     searchTextView.setText(getString(R.string.search_results, searchQuery));
+
+                    for (Book bookItem:searchList.getBooks()){
+                        Log.d("DEBUG", "Je rentre dans la boucle" + books.toString());
+                        String authorKey = bookItem.getAuthorKey();
+                        if(!AuthorData.getInstance().haveAuthor(authorKey)){
+                            GetRequest getRequestAuthor = new GetRequest();
+
+                            getRequestAuthor.retrAuthor(authorKey, new AuthorGetRequestCallback() {
+                                @Override
+                                public void onSuccess(Author author) {
+                                    author.setKey(authorKey);
+                                    Log.d("DEBUG","OnSuccees Author "+author.toString());
+                                    AuthorData.getInstance().addAuthor(author);
+                                }
+
+                                @Override
+                                public void onFailure(Throwable t) {
+                                    Log.e("DEBUG","Onfailure Author ");
+                                }
+                            });
+                        }
+                    }
+
                 }
 
                 @Override
